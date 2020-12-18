bda62ee945ee09d61d08a6abc2e74e3d0547bea0
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-18 15:49:26 -0600

    found some bugs

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 
 
 function filterByQuery(query, animalsArray) {
@@ -58,7 +60,7 @@ function findById(id, animalsArray) {
 
 function createNewAnimal(body, animalsArray) {
     const animal = body;
-    animalsArray.push(animal);
+    animals.push(animal);
     fs.writeFileSync(
         path.join(__dirname, './data/animals.json'),
         JSON.stringify({ animals: animalsArray }, null, 2)
@@ -100,6 +102,18 @@ app.get('/api/animals/:id', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
 app.post('/api/animals', (req, res) => {
     /// set id based on what the next index of the array will be
     req.body.id = animals.length.toString();
@@ -111,6 +125,10 @@ app.post('/api/animals', (req, res) => {
         const animal = createNewAnimal(req.body, animals);
         res.json(animal);
     }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
