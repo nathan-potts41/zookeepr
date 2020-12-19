68204ec506ce712e40de288f401955f0ce5ef2fa
nathan-potts41 <pottsandpans41@hotmail.com>
2020-12-19 13:04:59 -0600

    added zookeeper feature

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
+const zookeepers = require('../../lib/zookeepers');
 const animalRoutes = require('../apiRoutes/animalRoutes');
 
 router.use(animalRoutes);
+router.use(require('./zookeeperRoutes'));
 
 module.exports = router;
